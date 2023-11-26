bde3830f665a68045efd3ad5dc7f26807b63db46
Verdiyevasema <verdiyevasma3@gmail.com>
2023-11-26 18:40:06 +0400

    ResponsiveNavbarTask

--- 05-ResponsiveNavbarTask/main.js
+++ 05-ResponsiveNavbarTask/main.js
@@ -1,17 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let menuIcon = document.getElementById('menu-icon');
-    let closeIcon = document.getElementById('close-icon');
-    let mainNav = document.getElementById('main-nav');
+document.addEventListener("DOMContentLoaded", function () {
+  let menuIcon = document.getElementById("menu-icon");
+  let closeIcon = document.getElementById("close-icon");
+  let mainNav = document.getElementById("main-nav");
+  let header = document.querySelector("header");
+  window.addEventListener("scroll", function () {
+    this.window.scrollY > 0
+      ? header.classList.add("header-scroll")
+      : header.classList.remove("header-scroll");
+  });
 
-    menuIcon.addEventListener('click', function () {
-        mainNav.classList.toggle('show');
-        menuIcon.style.display = 'none';
-        closeIcon.style.display = 'block';
-    });
+  window.addEventListener("scroll", function () {
+    console.log("window.scrollY");
+    header.classList.toggle("header-scroll", this.window.scrollY > 0);
+  });
+  menuIcon.addEventListener("click", function () {
+    mainNav.classList.toggle("show");
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  });
 
-    closeIcon.addEventListener('click', function () {
-        mainNav.classList.toggle('show');
-        menuIcon.style.display = 'block';
-        closeIcon.style.display = 'none';
-    });
+  closeIcon.addEventListener("click", function () {
+    mainNav.classList.toggle("show");
+    menuIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  });
 });
