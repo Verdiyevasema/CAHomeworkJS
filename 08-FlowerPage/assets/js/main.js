92c240839272f4e72eb0ce1770477cd1f0e0afb6
Verdiyevasema <verdiyevasma3@gmail.com>
2023-12-20 03:50:26 +0400

    Floral Studio Website

--- 08-FlowerPage/assets/js/main.js
+++ 08-FlowerPage/assets/js/main.js
@@ -26,3 +26,42 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+let flowersPricing = document.querySelector(".flowersPricing");
+let container = document.querySelector(".container");
+let wonderfulBeauty = document.querySelector(".wonderfulBeauty");
+let flowerImg = document.querySelector(".flowerImg");
+let limit = 3;
+let copyData = [];
+let leadMoreBtn = document.querySelector(".leadMoreBtn");
+const BASE_URL = "http://localhost:8080";
+
+async function getData(endpoint) {
+  const resp = await axios(`${BASE_URL}/${endpoint}`);
+  console.log(resp.data);
+  copyData = resp.data;
+  drawCards(resp.data.slice(0, limit));
+}
+
+getData("flowersPricing");
+
+function drawCards(array) {
+  flowerImg.innerHTML = "";
+  console.log(array);
+  array.forEach((el) => {
+    flowerImg.innerHTML += `
+    <div class="flowerImg1">
+    <img src="${el.photo}" alt="" />
+    <div>
+      <h6>${el.name}</h6>
+      <p>$${el.price}</p>
+    </div>
+  </div>
+        `;
+  });
+}
+
+leadMoreBtn.addEventListener("click", function () {
+  limit += 3;
+  drawCards(copyData.slice(0, limit));
+});
