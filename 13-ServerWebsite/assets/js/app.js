0ba70cba54ecd92b6a82822fe62541930e5d66f6
Verdiyevasema <verdiyevasma3@gmail.com>
2024-01-12 04:02:21 +0400

    Server Website Project

--- 13-ServerWebsite/assets/js/app.js
+++ 13-ServerWebsite/assets/js/app.js
@@ -1,16 +1,12 @@
-let header = document.querySelector("header");
-let nav = document.querySelector("nav");
-let inputSelect = document.querySelector(".inputSelect");
-let ourBestServices = document.querySelector(".ourBestServices");
+let ourProduct = document.querySelector(".ourProduct");
 let container = document.querySelector(".container");
-let ourTopServices = document.querySelector(".ourTopServices");
-let ourTopServicesCards = document.querySelector(".ourTopServicesCards");
-let allBtn = document.querySelectorAll(".btn");
-let cards = document.querySelector(".cards");
+let productsSection = document.querySelector(".productsSection");
+let chooseCards = document.querySelector(".chooseCards");
+let leadMoreBtn = document.querySelector(".leadMoreBtn");
+let choosecard = document.querySelector(".choosecard");
 let limit = 3;
 let copyData = [];
-let leadMoreBtn = document.querySelector(".leadMoreBtn");
-const BASE_URL = "  http://localhost:8080";
+const BASE_URL = "http://localhost:8000";
 
 async function getData(endpoint) {
   const resp = await axios(`${BASE_URL}/${endpoint}`);
@@ -19,44 +15,38 @@ async function getData(endpoint) {
   drawCards(resp.data.slice(0, limit));
 }
 
-getData("ourBestServices");
+getData("whyChooseUs");
 
 function drawCards(array) {
-  ourTopServicesCards.innerHTML = "";
+  chooseCards.innerHTML = "";
   console.log(array);
   array.forEach((el) => {
-    ourTopServicesCards.innerHTML += `
-    
-         <div class="cards">
+    chooseCards.innerHTML += `
+         <div class="choosecard">
             <img src="${el.photo}" alt="" />
             <div>
-              <h2>${el.name}</h2>
+              <h6>${el.name}</h6>
               <p>${el.text}</p>
-              <button  class="btn btn-danger" onclick="deletecard(${el.id}, this)">Delete</button>
-        
             </div>
           </div>
+          <button class="btn btn-danger" onclick="deleteProduct(${el.id},this)">Delete</button>
         `;
   });
 }
 
-search.addEventListener("input", async function (event) {
-  let response = await axios(`${BASE_URL}/ourBestServices`);
-  let filtered = response.data.filter((item) =>
-    item.name
-      .toLocaleLowerCase()
-      .includes(event.target.value.toLocaleLowerCase())
-  );
-  drawCards(filtered);
-});
-
-
-
-async function deletecard(id, btn) {
-  console.log(id,btn);
-  if (confirm("Are you sure you want to delete it?")) {
-    btn.closest(".cards").remove();
-await axios.delete(`${BASE_URL}/ourBestServices/${id}`);
+async function deleteProduct(id, btn) {
+  console.log(id, btn);
+  try {
+    if (window.confirm("Silmek istediyinize eminsiniz?")) {
+      await axios.delete(`${BASE_URL}/whyChooseUs/${id}`);
+      btn.closest(".choosecard").remove();
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
+leadMoreBtn.addEventListener("click", function () {
+  limit += 3;
+  drawCards(copyData.slice(0, limit));
+});
